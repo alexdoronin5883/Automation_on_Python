b858b7794c79d0b69ce206e6a025aa0cc0c27fa5
Александр Доронин <107746307+alexdoronin5883@users.noreply.github.com>
2023-10-07 02:34:45 +0300

    Update test_task_2.py

--- test_task_2.py
+++ test_task_2.py
@@ -9,6 +9,11 @@ with open('testdata.yaml', encoding='utf-8') as f:
 
 
 def test_task_2():
+    """
+    get_user() получает "id"  с запросом post
+    user_info получает "id" с запросом get
+    assert срвнивает значения 
+    """
     logging.info("test_rest_api Starting")
     checkers = ApiHelper()
     checkers.get_user()
